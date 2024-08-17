d0fdcf883689b6e9b5a034324029ca955878f651
Dnevend <Dnevend@gmail.com>
2024-08-17 10:30:53 +0800

    styles: font

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -11,17 +11,17 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
   ({ className, type, icon, ...props }, ref) => {
     return (
       <div className="relative flex items-center">
+        {icon && <span className="absolute left-3">{icon}</span>}
         <input
           type={type}
           className={cn(
             "flex h-10 w-full rounded-md border border-input bg-transparent px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
-            icon ? "pr-10" : "", // Add padding to make room for the icon
+            icon ? "pl-10" : "", // Add padding to make room for the icon
             className
           )}
           ref={ref}
           {...props}
         />
-        {icon && <span className="absolute right-3">{icon}</span>}
       </div>
     );
   }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    fontFamily: {
+      'Roboto Slab': ['Roboto Slab', 'serif'],
+      lora: ['Lora', 'serif']
+    },
+    extend: {
+    },
   },
   plugins: [],
 }
