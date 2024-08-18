53d3676aa81d343a5ef29df8490d4b569c883210
Dnevend <Dnevend@gmail.com>
2024-08-18 10:48:47 +0800

    chore: remove connectkit

--- src/index.tsx
+++ src/index.tsx
@@ -3,10 +3,7 @@ import * as ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-// import { WagmiProvider } from "wagmi";
-// import { config } from "./config";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-// import { ConnectKitProvider } from "connectkit";
 
 const queryClient = new QueryClient();
 
@@ -23,12 +20,8 @@ const router = createBrowserRouter([
 
 root.render(
   <React.StrictMode>
-    {/* <WagmiProvider config={config}> */}
     <QueryClientProvider client={queryClient}>
-      {/* <ConnectKitProvider> */}
       <RouterProvider router={router} />
-      {/* </ConnectKitProvider> */}
     </QueryClientProvider>
-    {/* </WagmiProvider> */}
   </React.StrictMode>
 );
